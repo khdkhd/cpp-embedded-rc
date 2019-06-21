9b1590a53e8ab77826019d5d86e25746d36a23bf
NealRame <contact@nealrame.com>
2019-06-22 01:26:08 +0200

    Build only 64 bit and C++11

--- Scripts/CI/Conan/build.py
+++ Scripts/CI/Conan/build.py
@@ -6,6 +6,7 @@ def build():
         pure_c=False,
         shared_option_name="cpp-embedded-rc:shared"
     )
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
     builder.run()
 
 if __name__ == "__main__":
