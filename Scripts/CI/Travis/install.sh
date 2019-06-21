e0e7c7fec45327fdb2666593f74c2ba0125fd6c0
NealRame <contact@nealrame.com>
2019-06-22 00:37:51 +0200

    Comment macOS section

--- Scripts/CI/Travis/install.sh
+++ Scripts/CI/Travis/install.sh
@@ -3,27 +3,27 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    MACOS_HEADER_PKG="/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg"
-
-    if [ -f "$MACOS_HEADER_PKG" ];
-        sudo installer -pkg "$MACOS_HEADER_PKG" -target /
-    fi
-
-    brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 3.7.3
-    pyenv virtualenv 3.7.3 conan
-    pyenv rehash
-    pyenv activate conan
-fi
+# if [[ "$(uname -s)" == 'Darwin' ]]; then
+#     MACOS_HEADER_PKG="/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg"
+#
+#     if [ -f "$MACOS_HEADER_PKG" ];
+#         sudo installer -pkg "$MACOS_HEADER_PKG" -target /
+#     fi
+#
+#     brew update
+#     brew outdated pyenv || brew upgrade pyenv
+#     brew install pyenv-virtualenv
+#     brew install cmake || true
+#
+#     if which pyenv > /dev/null; then
+#         eval "$(pyenv init -)"
+#     fi
+#
+#     pyenv install 3.7.3
+#     pyenv virtualenv 3.7.3 conan
+#     pyenv rehash
+#     pyenv activate conan
+# fi
 
 pip install conan --upgrade
 pip install conan_package_tools
